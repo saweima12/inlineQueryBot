0a6e957b0e2ff4e7e59c2ce75ccca76bb67afa09
saweima12 <saweima12@gmail.com>
2022-08-24 18:02:43 +0800

    adjust startup app hoook

--- inlinebot/__init__.py
+++ inlinebot/__init__.py
@@ -1,23 +1,24 @@
-from sanic import Sanic, Request
-from aiogram.types import Message
-
+import os
+from sanic import Sanic
 from inlinebot.services import bot
 from inlinebot.view import bp
 
-# define sanic application.
-app = Sanic(__name__)
+from . import config
 
+# define sanic application.
+app = Sanic(__name__, env_prefix="INLINEBOT_")
+# load default config
+app.update_config(config)
+
+# load config on environment path.
+env_path = os.environ.get("_INLINEBOT_CONFIG")
+if env_path:
+    app.update_config(env_path)
 
 
 # register service.
 bot.setup(app)
 
-
-# initialize project
-@app.main_process_start
-async def main_startup():
-    pass
-
-
 # register route.
+app.static("/admin", "/static/admin")
 app.blueprint(bp)
