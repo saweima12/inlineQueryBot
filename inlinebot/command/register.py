d1d10d3ac6db2d27635d7ce5f4f306a751f38a0d
saweima12 <saweima12@gmail.com>
2022-08-26 18:16:02 +0800

    add command_map component

--- inlinebot/command/register.py
+++ inlinebot/command/register.py
@@ -1,5 +1,23 @@
+from sanic import Sanic
+from sanic.log import logger
 from aiogram.types import Message
-from meilisearch_python_async import Client
+from inlinebot.models import WhiteListConfig
+from inlinebot.services.meili import get_client
 
-async def register_user(*params, message: Message, search: Client, **options):
-    pass
+async def register_user(*params, message: Message, **options):
+    # get authencitcate params
+    REGISTER_KEY = Sanic.get_app().config.get("REGISTRY_KEY", "")
+    if len(params) != 1:
+        return
+
+    # validate authenticate_key
+    authenticate_key = params[0]
+    if authenticate_key != REGISTER_KEY:
+        return
+
+
+    client = get_client()
+    # get user_id
+    user_id = str(message.from_user.id)
+    await WhiteListConfig.add_user(user_id, client)
+    logger.info(f"on register user: {user_id}")
